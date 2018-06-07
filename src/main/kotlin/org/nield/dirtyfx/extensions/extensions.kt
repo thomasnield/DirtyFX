369310f9d451fd30f6c6d8c87ba2a36077c5239c
Thomas Nield <thomasnield@live.com>
2018-06-07 13:45:27 -0500

    make CompositeDirtyProperty implement ObservableValue<Boolean>

--- src/main/kotlin/org/nield/dirtyfx/extensions/extensions.kt
+++ src/main/kotlin/org/nield/dirtyfx/extensions/extensions.kt
@@ -2,11 +2,8 @@ package org.nield.dirtyfx.extensions
 
 import org.nield.dirtyfx.tracking.CompositeDirtyProperty
 import org.nield.dirtyfx.tracking.DirtyProperty
-import kotlin.reflect.KProperty
 
 fun DirtyProperty.addTo(compositeDirtyProperty: CompositeDirtyProperty): DirtyProperty {
     compositeDirtyProperty.add(this)
     return this
 }
-
-operator fun CompositeDirtyProperty.getValue(thisRef: Any, property: KProperty<*>) = isDirty
--- src/main/kotlin/org/nield/dirtyfx/tracking/CompositeDirtyProperty.kt
+++ src/main/kotlin/org/nield/dirtyfx/tracking/CompositeDirtyProperty.kt
@@ -1,12 +1,14 @@
 package org.nield.dirtyfx.tracking
 
+import javafx.beans.InvalidationListener
 import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.value.ChangeListener
 import javafx.beans.value.ObservableValue
 import javafx.collections.FXCollections
 import javafx.collections.ListChangeListener
 import javafx.collections.WeakListChangeListener
 
-class CompositeDirtyProperty: DirtyProperty {
+class CompositeDirtyProperty: DirtyProperty, ObservableValue<Boolean> {
 
     private val items = FXCollections.observableArrayList<DirtyProperty> { arrayOf(it.isDirtyProperty()) }
     private val _dirtyStateProperty = SimpleBooleanProperty()
@@ -63,5 +65,16 @@ class CompositeDirtyProperty: DirtyProperty {
     operator fun minusAssign(dirtyProperty: DirtyProperty) {
         items += dirtyProperty
     }
+
+    override fun removeListener(listener: ChangeListener<in Boolean>?) = _dirtyStateProperty.removeListener(listener)
+
+    override fun removeListener(listener: InvalidationListener?) = _dirtyStateProperty.removeListener(listener)
+
+    override fun addListener(listener: ChangeListener<in Boolean>?) = _dirtyStateProperty.addListener(listener)
+
+    override fun addListener(listener: InvalidationListener?)  = _dirtyStateProperty.addListener(listener)
+
+    override fun getValue() = _dirtyStateProperty.value
+
 
 }
