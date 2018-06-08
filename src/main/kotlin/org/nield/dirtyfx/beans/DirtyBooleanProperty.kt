90ad7a8fb66f161c70f5b688c71d0456e8220e6c
Thomas Nield <thomasnield@live.com>
2018-06-08 10:42:48 -0500

    Update DirtyBooleanProperty.kt

--- src/main/kotlin/org/nield/dirtyfx/beans/DirtyBooleanProperty.kt
+++ src/main/kotlin/org/nield/dirtyfx/beans/DirtyBooleanProperty.kt
@@ -1,5 +1,6 @@
 package org.nield.dirtyfx.beans
 
+
 import javafx.beans.InvalidationListener
 import javafx.beans.property.BooleanProperty
 import javafx.beans.property.SimpleBooleanProperty
@@ -10,7 +11,7 @@ import org.nield.dirtyfx.tracking.DirtyProperty
 
 class DirtyBooleanProperty(initialValue: Boolean): BooleanProperty(), DirtyProperty {
 
-    private var originalValue = initialValue
+    private var _originalValueProperty = SimpleBooleanProperty(initialValue)
     private val _isDirtyProperty = SimpleBooleanProperty(false)
 
     private val delegate = SimpleBooleanProperty(initialValue)
@@ -18,22 +19,26 @@ class DirtyBooleanProperty(initialValue: Boolean): BooleanProperty(), DirtyPrope
     init {
         addListener(
                 WeakChangeListener<Boolean> (
-                    ChangeListener<Boolean> { _,_,_ ->
-                        _isDirtyProperty.set(originalValue != value)
-                    }
+                        ChangeListener<Boolean> { _,_,_ ->
+                            _isDirtyProperty.set(_originalValueProperty.get() != value)
+                        }
                 )
         )
     }
     /** Sets the current value to now be the "original" value **/
     override fun rebaseline() {
-        originalValue = value
+        _originalValueProperty.set(value)
         _isDirtyProperty.set(false)
     }
     /** Resets the current value to the "original" value **/
     override fun reset() {
-        value = originalValue
+        value = _originalValueProperty.get()
         _isDirtyProperty.set(false)
     }
+
+    fun originalValueProperty(): ObservableValue<Boolean> = _originalValueProperty
+    val originalValue get() = _originalValueProperty.get()
+    
     override fun isDirtyProperty(): ObservableValue<Boolean> = _isDirtyProperty
     override val isDirty get() = _isDirtyProperty.get()
 
@@ -58,4 +63,3 @@ class DirtyBooleanProperty(initialValue: Boolean): BooleanProperty(), DirtyPrope
     override fun bind(observable: ObservableValue<out Boolean>?) = delegate.bind(observable)
 
     override fun isBound() = delegate.isBound
-}
