de023d3645edcc0481d9d5398aadc2fce650f2dc
Thomas Nield <thomasnield@live.com>
2018-06-08 10:21:16 -0500

    Update DirtyObservableMap.kt

--- src/main/kotlin/org/nield/dirtyfx/collections/DirtyObservableMap.kt
+++ src/main/kotlin/org/nield/dirtyfx/collections/DirtyObservableMap.kt
@@ -1,6 +1,5 @@
 package org.nield.dirtyfx.collections
 
-
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.value.ObservableValue
 import javafx.collections.FXCollections
@@ -9,35 +8,35 @@ import javafx.collections.ObservableMap
 import javafx.collections.WeakMapChangeListener
 import org.nield.dirtyfx.tracking.DirtyProperty
 
-class DirtyObservableMap<K,V> private constructor(originalMap: Map<K,V>,
+class DirtyObservableMap<K,V> private constructor(_originalMap: Map<K,V>,
                                                   val currentMap: ObservableMap<K,V>):
         ObservableMap<K,V> by currentMap, DirtyProperty {
 
-    private val originalMap = FXCollections.observableMap(originalMap)
+    private val _originalMap = FXCollections.observableMap(_originalMap)
     private val _isDirtyProperty = SimpleBooleanProperty()
 
     constructor(originalMap: Map<K,V> = mapOf()): this(originalMap, FXCollections.observableMap(HashMap<K,V>(originalMap)))
 
     override fun rebaseline() {
-        originalMap.clear()
-        forEach { k, v -> originalMap[k] = v }
+        _originalMap.clear()
+        forEach { k, v -> _originalMap[k] = v }
         _isDirtyProperty.set(false)
     }
     override fun reset() {
         clear()
-        originalMap.forEach { k, v -> set(k,v) }
+        _originalMap.forEach { k, v -> set(k,v) }
         _isDirtyProperty.set(false)
     }
     init {
         addListener(
                 WeakMapChangeListener<K,V>(
                         MapChangeListener<K,V> {
-                            _isDirtyProperty.set(originalMap != this)
+                            _isDirtyProperty.set(_originalMap != this)
                         }
                 )
         )
     }
-    val baselineMap get() = FXCollections.unmodifiableObservableMap(originalMap)
+    val originalMap get() = FXCollections.unmodifiableObservableMap(_originalMap)
 
     override fun isDirtyProperty(): ObservableValue<Boolean> = _isDirtyProperty
     override val isDirty get() = _isDirtyProperty.get()
